34a0d374e39f85404800e28fcd9109c2e86642c0
cheeyau <cheeyau@live.com>
2023-04-23 14:14:06 +0200

    add todo

--- manage.py
+++ manage.py
@@ -25,11 +25,12 @@ def make_shell_context():
         User_roll=UserRoll_repo
         )
 
-@app.cli.command()
-def test():
-    """Runs the unit tests."""
-    tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
-    result = unittest.TextTestRunner(verbosity=2).run(tests)
-    if result.wasSuccessful():
-        return 0
-    return 1
+# TODO create test command for populate database
+# @app.cli.command()
+# def test():
+#     """Runs the unit tests."""
+#     tests = unittest.TestLoader().discover('app/test', pattern='test*.py')
+#     result = unittest.TextTestRunner(verbosity=2).run(tests)
+#     if result.wasSuccessful():
+#         return 0
+#     return 1
